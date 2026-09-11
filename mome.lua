61b9cc49dea10af6ce5aa7fd5d60f275883aaf9e
momoneta <despiertapapu184@gmail.com>
2026-09-11 17:36:16 -0300

    Update mome.lua

--- mome.lua
+++ mome.lua
@@ -143,7 +143,7 @@ toggle.Position = UDim2.new(0, 5, 0, -2)
 toggle.Rotation = 90
 toggle.Size = UDim2.new(0, 20, 0, 20)
 toggle.ZIndex = 2
-toggle.Image = ""
+toggle.Image = "rbxassetid://112527838728403"
 
 base.Name = "Base"
 base.Parent = bar

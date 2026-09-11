e28503d708f4a706fbcdac08b75ec3c32169916e
momoneta <despiertapapu184@gmail.com>
2026-09-11 17:52:51 -0300

    Update mome.lua

--- mome.lua
+++ mome.lua
@@ -143,7 +143,7 @@ toggle.Position = UDim2.new(0, 5, 0, -2)
 toggle.Rotation = 90
 toggle.Size = UDim2.new(0, 20, 0, 20)
 toggle.ZIndex = 2
-toggle.Image = "https://www.roblox.com/Thumbs/Asset.ashx?width=420&height=420&assetId=87894664457655"
+toggle.Image = "https://www.roblox.com/Thumbs/Asset.ashx?width=500&height=500&assetId=87894664457655"
 
 base.Name = "Base"
 base.Parent = bar
